f1105ff6631b8aeef87dca51dcbe17f8b319bc7d
Septiani Yahya <septiani18si@mahasiswa.pcr.ac.id>
2021-01-21 13:28:53 +0700

    navigation

---
+++ app/src/main/java/com/example/belajarnavigationdrawer/HalamanSend.kt
@@ -0,0 +1,22 @@
+package com.example.belajarnavigationdrawer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HalamanInbox.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HalamanSend : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_halaman_send,container,true)
+    }
+}
